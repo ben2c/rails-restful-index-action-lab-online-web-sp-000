6a26c24da14d3b04a9193ddb4d6c04f32aef2422
Benjamin Cheng <bencheng200@gmail.com>
2021-03-11 20:45:30 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :students, only: :index
+  #resources :students, only: :index
   
-  get 'index', to: 'student#index'
+  get 'index', to: 'students#index'
 
 end
